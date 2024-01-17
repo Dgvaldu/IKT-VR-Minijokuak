9de9b9561b67f86d7f42d48bfa1831b9850d88a5
Dgvaldu <146193571+Dgvaldu@users.noreply.github.com>
2024-01-17 14:19:59 +0100

    Comit 17/01

--- Assets/Scripts/BossGusanoAttack.cs
+++ Assets/Scripts/BossGusanoAttack.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class BossGusanoAttack : MonoBehaviour
 {
-    
+    [SerializeField]
+    float MeleeRate;
+
+    float lastMelee;
+
+    private PlayerHealth playerHealth;
+
+    private void Start()
+    {
+        playerHealth = GameObject.Find("Player").GetComponent<PlayerHealth>();
+    }
+ 
+    public void MeleeAttack()
+    {
+        if (lastMelee + MeleeRate <= Time.time)
+        {
+            Debug.Log("Mele Atraves del suelo");
+
+            lastMelee = Time.time;
+            playerHealth.TakeDamage(10);
+        }
+    }
 }
--- Assets/Scripts/BossGusanoControler.cs
+++ Assets/Scripts/BossGusanoControler.cs
@@ -4,5 +4,70 @@ using UnityEngine;
 
 public class BossGusanoControler : MonoBehaviour
 {
-   
+    [SerializeField]
+    float sightRange;
+    [SerializeField]
+    float chaseRange;
+    [SerializeField]
+    float attackRange;
+    [SerializeField]
+    float minDistance;
+
+    GameObject player;
+
+    [SerializeField]
+    Animator animator;
+
+    public bool isDead;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(gameObject.transform.position, chaseRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(gameObject.transform.position, attackRange);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(gameObject.transform.position, sightRange);
+    }
+
+    private void Update()
+    {
+        if (!isDead)
+        {
+            float distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
+            if (distance <= sightRange)
+            {
+                //animator.SetBool("Iswalking", false);                
+                animator.SetBool("IsAttacking", false);
+                if (distance <= chaseRange)
+                {
+                    //animator.SetBool("Iswalking", true);
+                    animator.SetBool("IsAttacking", true);
+                    animator.SetBool("IsPutazo", false);
+
+                    if (distance <= attackRange)
+                    {
+                        //Enemy tiro egiteko erreferentzia ejekutatu
+
+                        //animator.SetBool("Iswalking", false);
+                        animator.SetBool("IsPutazo", true);
+                        animator.SetBool("IsAttacking", false);
+                    }
+
+                }
+            }
+        }
+    }
+
+    public void MeleeAttack()
+    {
+        gameObject.GetComponent<BossAttack>().MeleeAttack();
+    }
+
 }
